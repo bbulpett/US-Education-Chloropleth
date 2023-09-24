46e2ab77bc0f53c5f6b982471209a02e3fa76d0b
Barnabas Bulpett <barnabasbulpett@gmail.com>
2023-09-23 21:38:40 -0400

    Set up basic script structure and stylesheet

---
+++ script.js
@@ -0,0 +1,13 @@
+let countyURL = "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json";
+let educationURL = "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json";
+
+// Initialize variables for data arrays that D3 will use to draw map on canvas
+let countyData;
+let educationData;
+
+// Reference to the canvas element
+let canvas = d3.select('#canvas');
+
+let drawMap = () => {
+ // TODO: Draw the map on the canvas
+}
